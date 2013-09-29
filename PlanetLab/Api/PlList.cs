02b7268aa21f7baccf5c30a860de5be5322fb0c8
Alex Bikfalvi <alex@bikfalvi.com>
2013-09-29 22:06:27 +0200

    Changed the PL list.

--- PlanetLab/Api/PlList.cs
+++ PlanetLab/Api/PlList.cs
@@ -17,6 +17,7 @@
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace PlanetLab.Api
 	/// A class representing a PlanetLab list.
 	/// </summary>
 	/// <typeparam name="T">The list item type.</typeparam>
-	public class PlList<T> : ConcurrentDictionary<int, T> where T : PlObject, new()
+	public class PlList<T> : ConcurrentList<T> where T : PlObject, new()
 	{
 		/// <summary>
 		/// Creates an empty PlanetLab address list.
@@ -74,7 +75,7 @@ namespace PlanetLab.Api
 				// Clear the list.
 				this.Clear();
 				// Update the items.
-				foreach (KeyValuePair<int, T> item in list)
+				foreach (T item in list)
 				{
 					this.Add(item);
 				}
@@ -104,10 +105,7 @@ namespace PlanetLab.Api
 					
 					T item = new T();
 					item.Parse(element.Value as XmlRpcStruct);
-					if (item.Id.HasValue)
-					{
-						this.Add(item.Id.Value, item);
-					}
+					this.Add(item);
 				}
 			}
 		}
