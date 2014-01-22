20a42c1ccc3084ff8f7e783c3d8be82c8de16f92
Alex Bikfalvi <alex@bikfalvi.com>
2014-01-23 00:01:04 +0100

    Stream bug fix.

--- PlanetLabManager/Controls/ControlSsh.cs
+++ PlanetLabManager/Controls/ControlSsh.cs
@@ -352,12 +352,10 @@ namespace PlanetLab.Controls
 				// Get the command data.
 				ThreadPool.QueueUserWorkItem((object state) =>
 				{
-					// Get the command output stream.
-					PipeStream stream = command.OutputStream as PipeStream;
 					// Set the stream as blocking.
-					stream.BlockLastReadBuffer = true;
+					command.OutputStream.BlockLastReadBuffer = true;
 					// Read the command data.
-					using (PipeReader reader = new PipeReader(stream))
+					using (PipeReader reader = new PipeReader(command.OutputStream))
 					{
 						// While the command is not completed.
 						while (!asyncResult.IsCompleted)
